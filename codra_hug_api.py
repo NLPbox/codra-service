819bc9aed1c2b1d391dc8bb66b6cb1cee6ca4fa5
Arne Neumann <nlpbox.programming@arne.cl>
2017-12-14 21:33:06 +0100

    cleanup: parameterised API file

--- codra_hug_api.py
+++ codra_hug_api.py
@@ -7,20 +7,22 @@ import hug
 import sh
 
 PARSER_PATH = '/opt/codra-rst-parser'
+PARSER_EXECUTABLE = 'codra.sh'
+INPUT_FILEPATH = '/tmp/input.txt'
+OUTPUT_FILEPATH = INPUT_FILEPATH+'.dis'
 
 
 @hug.post('/parse', output=hug.output_format.file)
 def call_parser(body, output_format: hug.types.text):
-    codra = sh.Command(os.path.join(PARSER_PATH, 'codra.sh'))
+    parser = sh.Command(os.path.join(PARSER_PATH, PARSER_EXECUTABLE))
 
     if 'input' in body:
         input_file_content = body['input']
-        with open('/tmp/input.txt', 'wb') as input_file:
+        with open(INPUT_FILEPATH, 'wb') as input_file:
             input_file.write(input_file_content)
         
-        parser_stdout = codra(input_file.name, _cwd=PARSER_PATH)
-        print(os.listdir('/tmp'))
-        return '/tmp/input.txt.dis'
+        parser_stdout = parser(input_file.name, _cwd=PARSER_PATH)
+        return OUTPUT_FILEPATH
     
     else:
         return {'body': body}
