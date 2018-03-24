42a1d5c3e195e50ee4d6df69b4e19c592107438d
Arne Neumann <github@arne.cl>
2018-03-24 23:38:52 +0100

    feat: return proper parser error message via API

--- codra_hug_api.py
+++ codra_hug_api.py
@@ -2,6 +2,7 @@
 import os
 import sys
 
+from falcon import HTTP_500
 import hug
 import sh
 
@@ -20,23 +21,34 @@ def process_data(request, response, resource):
 
 
 @hug.post('/parse', output=hug.output_format.file)
-def call_parser(body, output_format):
+def call_parser(body, response):
     parser = sh.Command(os.path.join(PARSER_PATH, PARSER_EXECUTABLE))
 
     if 'input' in body:
         input_file_content = body['input']
         with open(INPUT_FILEPATH, 'wb') as input_file:
             input_file.write(input_file_content)
+
+        try:
+            parser_stdout = parser(input_file.name, _cwd=PARSER_PATH)
         
-        parser_stdout = parser(input_file.name, _cwd=PARSER_PATH)
+            if body['output_format'] == b'nltk-tree-png':
+                dis2png = sh.Command('./convert.sh')
+                dis2png(OUTPUT_FILEPATH)
+                return OUTPUT_FILEPATH+'.png'
         
-        if output_format == b'nltk-tree-png':
-            dis2png = sh.Command('./convert.sh')
-            dis2png(OUTPUT_FILEPATH)
-            return OUTPUT_FILEPATH+'.png'
-        
-        else: # always fall back to the 'original' output format of the parser
-            return OUTPUT_FILEPATH
-    
+            else: # always fall back to the 'original' output format of the parser
+                return OUTPUT_FILEPATH
+        except sh.ErrorReturnCode_1 as err:
+            response.status = HTTP_500
+            trace = str(err.stderr, 'utf-8')
+            error_msg = "{0}\n\n{1}".format(err, trace).encode('utf-8')
+
+            import tempfile
+            temp = tempfile.NamedTemporaryFile(delete=False)
+            temp.write(error_msg)
+            temp.close()
+            return temp.name
+
     else:
         return {'body': body}
